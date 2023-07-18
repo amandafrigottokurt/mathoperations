467619e47423a6bd23aaaa55980acb05b9081102
Amanda Frigotto Kurt <122942443+amandafrigottokurt@users.noreply.github.com>
2023-07-18 20:31:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-/* 
-Bora praticar e rever tudo o que foi ensinado na aula? **💜**
-Nesse desafio, você irá solicitar ao usuário que ele insira **dois números** e, a partir daí, calcular:
-
-- [ ]  A soma dos dois números;
-- [ ]  A subtração dos dois números;
-- [ ]  A multiplicação dos dois números;
-- [ ]  A divisão dos dois números;
-- [ ]  O resto da divisão dos dois números;
-
-Fácil até aqui, né? Bora se desafiar? 
-
-- [ ]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
-- [ ]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~.
-
-Lembre-se de sempre mostrar na tela o resultado de cada uma dessas operações!
-*/
-
 alert("Iremos realizar alguns cálculos com 2 números")
 let firstNumber = prompt('Digite o primeiro número')
 let secondNumber = prompt('Digite o segundo número')
